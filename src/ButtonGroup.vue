dc9d2560908566bdbcf7632644fc114ab204f1ec
cennavi <zhuwei_leo@163.com>
2018-07-11 14:10:04 +0800

    完成button-group组件

---
+++ src/ButtonGroup.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="zw-button-group">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ButtonGroup',
+        mounted() {
+            for(let node of this.$el.children){
+                if(node.tagName.toLowerCase() !== 'button') console.warn('按钮组的子元素必须是zw-button!')
+            }
+        }
+    }
+</script>
+
+<style lang='scss'>
+    .zw-button-group {
+        font-size: 0;
+        .zw-button {
+            border-radius: 0;
+            vertical-align: middle;
+
+            &:first-child {
+                border-top-left-radius: var(--border-radius, 4px);
+                border-bottom-left-radius: var(--border-radius, 4px);
+            }
+            &:last-child {
+                border-top-right-radius: var(--border-radius, 4px);
+                border-bottom-right-radius: var(--border-radius, 4px);
+            }
+            &:not(:first-child) {
+                margin-left: -1px;
+            }
+            &:hover {
+                position: relative;
+                z-index: 1;
+            }
+        }
+    }
+</style>
--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 
 import Button from './Button'
 import Icon from './Icon'
+import ButtonGroup from './ButtonGroup'
 
 Vue.component('zw-button', Button);
 Vue.component('zw-icon', Icon);
+Vue.component('zw-button-group', ButtonGroup);
 
 new Vue({
     el:'#app',
